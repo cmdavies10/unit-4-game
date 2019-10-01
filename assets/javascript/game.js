f7812f7dca6be416fcdf7a11836ae8b850e738a5
Chris Davies <c.m.davies10@gmail.com>
2019-10-01 15:04:20 -0400

    working version of game, need to add reset functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,23 +8,50 @@ $(document).ready(function () {
     var wins = 0;
     var losses = 0;
 
+    console.log(crystalOne);
+    console.log(crystalTwo);
+    console.log(crystalThree);
+    console.log(crystalFour);
+    console.log("=======");
+    console.log(total);
+
     // generate a beginning value
     $("#random-number").text(computerGuess);
     console.log(computerGuess);
 
+    // update total scores based on button clicks
     $("#crystal1").on("click", function () {
         total = total + crystalOne
         $("#total-score-text").text(total);
     });
 
-    // $(".btn").on("click", function () {
-    //     var userGuess = Math.floor((Math.random() * 12) + 1);
-    //     console.log(userGuess);
-    //     total = total + userGuess;
-    //     console.log(total);
-    // })
+    $("#crystal2").on("click", function () {
+        total = total + crystalTwo
+        $("#total-score-text").text(total);
+    });
+
+    $("#crystal3").on("click", function () {
+        total = total + crystalThree
+        $("#total-score-text").text(total);
+    });
+
+    $("#crystal4").on("click", function () {
+        total = total + crystalFour
+        $("#total-score-text").text(total);
+    });
+
+    // reset function
     
 
 
-
+    // decide and display wins & losses
+    $(".btn").on("click", function () {        
+        if (total === computerGuess) {
+            wins++;
+            $("#wins-text").text("Wins: " + wins);
+        } else if (total > computerGuess) {
+            losses++;
+            $("#losses-text").text("Losses: " + losses);
+        }
+    });
 })
